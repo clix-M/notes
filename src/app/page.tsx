464cd68eed6aceede9d37aefca3701240e536387
clix-M <clixdev07@gmail.com>
2024-05-04 21:29:26 -0500

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Home() {
+  return (
+    <main>
+      hola 
+      git config --global user.email "clixdev07@gmail.com"
+  git config --global user.name "clix-M"
+    </main>
+  );
+}
